de23035df429e778ca03f28a80174c2310f50047
leeteukki <tklohj@naver.com>
2017-07-08 16:06:42 +0900

    Design app

--- filight_booking/ViewController.swift
+++ filight_booking/ViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var departrueDateBtn: UIButton!
 
+    @IBOutlet var returnDateLa: UILabel!
+    @IBOutlet var returnDateBtn: UIButton!
+    
+    @IBOutlet var SelectDatePicker: UIDatePicker!
+    
+    @IBAction func showDatePicker(_ sender: Any) {
+        SelectDatePicker.isHidden = false
+    }
+    
+    @IBAction func selectedDateAction(_ sender: Any) {
+    }
+    
+    @IBAction func showReturnDateAction(_ sender: Any) {
+        print((sender as AnyObject).description!)
+        
+        /*
+        if (sender as! UISwitch).isOn {
+            returnDateLa.isHidden  = false
+            returnDateBtn.isHidden = false
+        }else{
+            returnDateLa.isHidden  = true
+            returnDateBtn.isHidden = true
+        }
+ */
+        returnDateLa.isHidden  = !(sender as! UISwitch).isOn
+        returnDateBtn.isHidden = !(sender as! UISwitch).isOn
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        returnDateLa.isHidden = true
+        returnDateBtn.isHidden = true
+        SelectDatePicker.isHidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
