281284b1d3a1aec05b2b0087e0360e33c0920084
leeteukki <tklohj@naver.com>
2017-07-08 14:59:02 +0900

    Initial Commit

---
+++ filight_booking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  filight_booking
+//
+//  Created by Lena Lee on 2017. 7. 8..
+//  Copyright © 2017년 Lena Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
